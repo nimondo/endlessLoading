c206372dea0d6dc3c94fb26378ce14e48a42a954
Znimed <znimed@gmail.com>
2020-05-19 17:48:56 +0000

    Alter adapter

--- app/src/main/java/com/example/loadmore/ui/post/PostListAdapter.kt
+++ app/src/main/java/com/example/loadmore/ui/post/PostListAdapter.kt
@@ -1,30 +1,59 @@
 package com.example.loadmore.ui.post
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import com.example.loadmore.R
 import com.example.loadmore.BR
+import com.example.loadmore.Constant
 import com.example.loadmore.data.model.Post
 import com.example.loadmore.databinding.ItemPostListBinding
+import com.example.loadmore.databinding.ProgressLoadingBinding
+import kotlinx.android.synthetic.main.progress_loading.view.*
 
 class PostListAdapter(val viewModel: PostViewModel) :
-    RecyclerView.Adapter<PostListAdapter.ViewHolder>() {
+    Adapter<RecyclerView.ViewHolder>() {
     private lateinit var postList: List<Post>
-
+    override fun getItemViewType(position: Int): Int {
+        return if (postList[position] == null) Constant.VIEW_TYPE_LOADING
+        else super.getItemViewType(position)
+    }
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ViewHolder {
+    ):  ViewHolder {
         val binding: ItemPostListBinding = DataBindingUtil.inflate(
             LayoutInflater.from(parent.context),
             R.layout.item_post_list,
             parent,
             false
         )
-        return ViewHolder(binding)
+        return if (viewType == Constant.VIEW_TYPE_ITEM) {
+            ViewHolder(binding)
+        }else{
+            val progressingBinding:ProgressLoadingBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.progress_loading, parent,
+                false
+            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                progressingBinding.progressbar.indeterminateDrawable.colorFilter = BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+            } else {
+                progressingBinding.progressbar.indeterminateDrawable.setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
+            }
+            LoadingViewHolder(progressingBinding)
+            ViewHolder(progressingBinding)
+//            ViewHolder(binding)
+        }
+//        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PostListAdapter.ViewHolder, position: Int) {
@@ -50,4 +79,5 @@ class PostListAdapter(val viewModel: PostViewModel) :
             binding.setVariable(BR.post, posts)
         }
     }
+    class LoadingViewHolder(itemView: ProgressLoadingBinding) : RecyclerView.ViewHolder(itemView.root)
 }
